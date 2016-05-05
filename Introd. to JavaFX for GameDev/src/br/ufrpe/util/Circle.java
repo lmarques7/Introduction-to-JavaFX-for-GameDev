2963111237bfa7120eb098cf40a3c6f61f9831e3
lmarques7 <leandro.marques@gmail.com>
2016-05-05 16:01:20 -0300

    Update Circle.java

--- Introd. to JavaFX for GameDev/src/br/ufrpe/util/Circle.java
+++ Introd. to JavaFX for GameDev/src/br/ufrpe/util/Circle.java
@@ -2,7 +2,7 @@ package br.ufrpe.util;
 
 import javafx.geometry.Point2D;
 
-// Represents a circle
+// Represents a 2D circle 
 public class Circle {
 	private Point2D center;
 	private double radius;
